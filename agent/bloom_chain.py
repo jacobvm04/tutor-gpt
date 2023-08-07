5f1cdb02456c539cfa05e7fae4b2b8196ed2f7e3
Ayush <61765149+citricacid2@users.noreply.github.com>
2023-08-06 21:41:36 -0400

    intermediate tool

--- agent/bloom_chain.py
+++ agent/bloom_chain.py
@@ -12,9 +12,15 @@ from langchain.agents import load_tools
 from langchain.tools import format_tool_to_openai_function
 
 
-SYSTEM_THOUGHT = load_prompt(os.path.join(os.path.dirname(__file__), 'data/prompts/system/thought.yaml'))
-SYSTEM_RESPONSE = load_prompt(os.path.join(os.path.dirname(__file__), 'data/prompts/system/response.yaml'))
-SYSTEM_TOOLS = load_prompt(os.path.join(os.path.dirname(__file__), 'data/prompts/system/tools.yaml'))
+SYSTEM_THOUGHT = load_prompt(
+    os.path.join(os.path.dirname(__file__), "data/prompts/system/thought.yaml")
+)
+SYSTEM_RESPONSE = load_prompt(
+    os.path.join(os.path.dirname(__file__), "data/prompts/system/response.yaml")
+)
+SYSTEM_TOOLS = load_prompt(
+    os.path.join(os.path.dirname(__file__), "data/prompts/system/tools.yaml")
+)
 
 
 class BloomChain:
@@ -23,114 +29,182 @@ class BloomChain:
         self.verbose = verbose
 
         # setup prompts
-        self.system_thought = SystemMessagePromptTemplate(prompt=SYSTEM_THOUGHT).format()
-        self.system_tools = SystemMessagePromptTemplate(prompt=SYSTEM_TOOLS).format()
+        self.system_thought = SystemMessagePromptTemplate(prompt=SYSTEM_THOUGHT)
+        self.system_tools = SystemMessagePromptTemplate(prompt=SYSTEM_TOOLS)
         self.system_response = SystemMessagePromptTemplate(prompt=SYSTEM_RESPONSE)
 
         # setup tools
         # TODO: setup tool retreival
-        self.tools = load_tools(["google-serper", "wolfram-alpha", "wikipedia", "arxiv"], llm=llm)
-        self.tools_openai = [format_tool_to_openai_function(tool) for tool in self.tools]
+        self.tools = load_tools(
+            ["google-serper", "wolfram-alpha", "wikipedia", "arxiv"], llm=llm
+        )
+        self.tools_openai = [
+            format_tool_to_openai_function(tool) for tool in self.tools
+        ]
         self.tool_names = [tool.name for tool in self.tools]
-        
+        self.tool_info = [f"{tool.name}: {tool.description}" for tool in self.tools]
 
-    def think(self, thought_memory: ChatMessageHistory, input: str, max_turns: int = 10):
+    async def think(
+        self, thought_memory: ChatMessageHistory, input: str, max_turns: int = 10
+    ):
         """Generate Bloom's thought on the user."""
+        messages = [
+            self.system_thought.format(tools="\n".join(self.tool_info)),
+            *thought_memory.messages,
+            HumanMessage(content=input),
+        ]
+        thought_message = await self.llm.apredict_messages(messages)
 
-        # load message history
-        messages = [self.system_thought, *thought_memory.messages, HumanMessage(content=input), self.system_tools]
-
-        # Builds the thought, makes sure it encompasses the student's needs as well as the necessary information to help the student
-        # 
-        # It works by letting the model first use tools to gain the necessary knowledge to answer the question, and then forming the thought
-        # More technically, this loop is just checking for each of the 3 possible messages from the model: Reasoning for using a tool, a tool call, and the final thought
-        turns = 0
-        while True:
-            # Check if we've reached the max number of turns
-            turns += 1
-            if turns > max_turns:
-                # reset messages and come up with thought without tools
-                messages = [self.system_thought, *thought_memory.messages, HumanMessage(content=input)]
-                thought_message = self.llm.predict_messages(messages)
-
-                # verbose logging
-                if self.verbose:
-                    # Seralize messages to strings
-                    message_strings = [f"{message.type}: {message.content}" for message in messages]
-                    print("Thought Conversation: ```\n", "\n\n".join(message_strings), "\n```\n")
-
-                    print("New Thought (Tools Failed): ```\n", thought_message.content, "\n```\n")
-
-                # update chat memory and return
-                thought_memory.add_message(HumanMessage(content=input))
-                thought_memory.add_message(thought_message)
-
-                return thought_message.content
-
-            thought_message = self.llm.predict_messages(messages, functions=self.tools_openai)
-
-            # Check if the model is reasoning about a function call
-            if thought_message.content.startswith("Reasoning:"):
-                if self.verbose:
-                    print("Reasoning: ```", thought_message.content, "```")
-
-                if "function_call" in thought_message.additional_kwargs:
-                    pass
-                else:
-                    messages.append(thought_message)
-                    continue
-
-            # Handle function calls
-            if "function_call" in thought_message.additional_kwargs:
-                # get the tool
-                tool_name = thought_message.additional_kwargs["function_call"]["name"]
-                tool = [tool for tool in self.tools if tool.name == tool_name][0]
-                arguments = json.loads(thought_message.additional_kwargs["function_call"]["arguments"])
-                
-                # call the tool
-                result = tool(arguments["__arg1"])
-                result_message = FunctionMessage(name=tool_name, content=result)
-
-                messages.append(thought_message)
-                messages.append(result_message)
-
-                # verbose logging
-                if self.verbose:
-                    print(f"Function call: ```{tool_name}({arguments['__arg1']})```")
-                    print(f"Result: ```{result}```\n")
-
-                continue
-
-            # verbose logging
-            if self.verbose:
-                # Seralize messages to strings
-                message_strings = [f"{message.type}: {message.content}" for message in messages]
-                print("Thought Conversation: ```\n", "\n\n".join(message_strings), "\n```\n")
-
-                print("New Thought: ```\n", thought_message.content, "\n```\n")
-
-            break
-
-        # update chat memory
+        # add thought to memory
         thought_memory.add_message(HumanMessage(content=input))
         thought_memory.add_message(thought_message)
 
+        # verbose logging
+        if self.verbose:
+            # Seralize messages to strings
+            message_strings = [
+                f"{message.type}: {message.content}" for message in messages
+            ]
+            print(
+                "Thought Conversation: ```\n", "\n\n".join(message_strings), "\n```\n"
+            )
+
+            print("New Thought: ```\n", thought_message.content, "\n```\n")
+
         return thought_message.content
-    
+
+        # # load message history
+        # messages = [self.system_thought, *thought_memory.messages, HumanMessage(content=input), self.system_tools]
+
+        # # Builds the thought, makes sure it encompasses the student's needs as well as the necessary information to help the student
+        # #
+        # # It works by letting the model first use tools to gain the necessary knowledge to answer the question, and then forming the thought
+        # # More technically, this loop is just checking for each of the 3 possible messages from the model: Reasoning for using a tool, a tool call, and the final thought
+        # turns = 0
+        # while True:
+        #     # Check if we've reached the max number of turns
+        #     turns += 1
+        #     if turns > max_turns:
+        #         # reset messages and come up with thought without tools
+        #         messages = [self.system_thought, *thought_memory.messages, HumanMessage(content=input)]
+        #         thought_message = self.llm.predict_messages(messages)
+
+        #         # verbose logging
+        #         if self.verbose:
+        #             # Seralize messages to strings
+        #             message_strings = [f"{message.type}: {message.content}" for message in messages]
+        #             print("Thought Conversation: ```\n", "\n\n".join(message_strings), "\n```\n")
+
+        #             print("New Thought (Tools Failed): ```\n", thought_message.content, "\n```\n")
+
+        #         # update chat memory and return
+        #         thought_memory.add_message(HumanMessage(content=input))
+        #         thought_memory.add_message(thought_message)
+
+        #         return thought_message.content
+
+        #     thought_message = self.llm.predict_messages(messages, functions=self.tools_openai)
+
+        #     # Check if the model is reasoning about a function call
+        #     if thought_message.content.startswith("Reasoning:"):
+        #         if self.verbose:
+        #             print("Reasoning: ```", thought_message.content, "```")
+
+        #         if "function_call" in thought_message.additional_kwargs:
+        #             pass
+        #         else:
+        #             messages.append(thought_message)
+        #             continue
+
+        #     # Handle function calls
+        #     if "function_call" in thought_message.additional_kwargs:
+        #         # get the tool
+        #         tool_name = thought_message.additional_kwargs["function_call"]["name"]
+        #         tool = [tool for tool in self.tools if tool.name == tool_name][0]
+        #         arguments = json.loads(thought_message.additional_kwargs["function_call"]["arguments"])
+
+        #         # call the tool
+        #         result = tool(arguments["__arg1"])
+        #         result_message = FunctionMessage(name=tool_name, content=result)
+
+        #         messages.append(thought_message)
+        #         messages.append(result_message)
+
+        #         # verbose logging
+        #         if self.verbose:
+        #             print(f"Function call: ```{tool_name}({arguments['__arg1']})```")
+        #             print(f"Result: ```{result}```\n")
+
+        #         continue
+
+        #     # verbose logging
+        #     if self.verbose:
+        #         # Seralize messages to strings
+        #         message_strings = [f"{message.type}: {message.content}" for message in messages]
+        #         print("Thought Conversation: ```\n", "\n\n".join(message_strings), "\n```\n")
+
+        #         print("New Thought: ```\n", thought_message.content, "\n```\n")
+
+        #     break
+
+        # # update chat memory
+        # thought_memory.add_message(HumanMessage(content=input))
+        # thought_memory.add_message(thought_message)
+
+        # return thought_message.content
+
+    async def collect_data(self, thought: str, input: str):
+        """Utilize tools to collect data for Bloom's response."""
+        messages = [
+            self.system_tools.format(thought=thought, tools="\n".join(self.tool_info)),
+            HumanMessage(content=input),
+        ]
+
+        while True:
+            output = await self.llm.apredict_messages(
+                messages, functions=self.tools_openai
+            )
+            print(output.content)
+            if "function_call" in output.additional_kwargs:
+                tool_name = output.additional_kwargs["function_call"]["name"]
+                tool = [tool for tool in self.tools if tool.name == tool_name][0]
+                arguments = json.loads(
+                    output.additional_kwargs["function_call"]["arguments"]
+                )
+                result = tool(arguments["__arg1"])
+                result_message = FunctionMessage(name=tool_name, content=result)
+                print(result_message.content)
+                messages.append(output)
+                messages.append(result_message)
+                continue
+            if output.content.startswith("REFLECTION:"):
+                messages.append(output)
+                continue
+            if output.content.startswith("SUMMARIZE:"):
+                return output.content.split("SUMMARIZE:")[1].strip()
 
     def respond(self, response_memory: ChatMessageHistory, thought: str, input: str):
         """Generate Bloom's response to the user."""
 
         # load message history
         # TODO: Is the "thought: " prefix necessary?
-        messages = [self.system_response.format(tools=self.tool_names), *response_memory.messages, HumanMessage(content=input), AIMessage(content=thought)]
+        messages = [
+            self.system_response.format(tools=self.tool_names),
+            *response_memory.messages,
+            HumanMessage(content=input),
+            AIMessage(content=thought),
+        ]
         response_message = self.llm.predict_messages(messages)
 
         # verbose logging
         if self.verbose:
             # Seralize messages to strings
-            message_strings = [f"{message.type}: {message.content}" for message in messages]
-            print("Response Conversation: ```\n", "\n\n".join(message_strings), "\n```\n")
+            message_strings = [
+                f"{message.type}: {message.content}" for message in messages
+            ]
+            print(
+                "Response Conversation: ```\n", "\n\n".join(message_strings), "\n```\n"
+            )
 
             print("New Response: ```\n", response_message.content, "\n```\n")
 
@@ -139,4 +213,3 @@ class BloomChain:
         response_memory.add_message(response_message)
 
         return response_message.content
-    
--- agent/chain.py
+++ agent/chain.py
@@ -11,7 +11,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-
 def load_memories():
     """Load the memory objects"""
     thought_memory: ChatMessageHistory
@@ -21,31 +20,27 @@ def load_memories():
     thought_memory = ChatMessageHistory()
     response_memory = ChatMessageHistory()
 
-
     return (thought_memory, response_memory)
 
 
 def load_chains():
     """Logic for loading the chain you want to use should go here."""
-    llm = ChatOpenAI(model_name = "gpt-4-0613", temperature=1.2)
+    llm = ChatOpenAI(model_name="gpt-4-0613", temperature=1.2)
 
     # define chain
-    bloom_chain = BloomChain(
-        llm=llm, 
-        verbose=True
-    )
+    bloom_chain = BloomChain(llm=llm, verbose=True)
 
     return bloom_chain
 
 
 async def chat(**kwargs):
     # if we sent a thought across, generate a response
-    if kwargs.get('thought'):
-        assert kwargs.get('bloom_chain'), "Please pass the bloom chain."
-        response_chain: BloomChain = kwargs.get('bloom_chain')
-        response_memory: ChatMessageHistory = kwargs.get('response_memory')
-        inp = kwargs.get('inp')
-        thought = kwargs.get('thought')
+    if kwargs.get("thought"):
+        assert kwargs.get("bloom_chain"), "Please pass the bloom chain."
+        response_chain: BloomChain = kwargs.get("bloom_chain")
+        response_memory: ChatMessageHistory = kwargs.get("response_memory")
+        inp = kwargs.get("inp")
+        thought = kwargs.get("thought")
 
         # get the history into a string
         # history = response_memory.load_memory_variables({})['history']
@@ -58,42 +53,44 @@ async def chat(**kwargs):
 
         response = response_chain.respond(response_memory, thought, inp)
 
-        if 'Student:' in response:
-            response = response.split('Student:')[0].strip()
-        if 'Studen:' in response:
-            response = response.split('Studen:')[0].strip()
+        if "Student:" in response:
+            response = response.split("Student:")[0].strip()
+        if "Studen:" in response:
+            response = response.split("Studen:")[0].strip()
 
         return response
 
     # otherwise, we're generating a thought
     else:
-        assert kwargs.get('bloom_chain'), "Please pass the thought chain."
-        inp = kwargs.get('inp')
-        thought_chain: BloomChain = kwargs.get('bloom_chain')
-        thought_memory: ChatMessageHistory = kwargs.get('thought_memory')
+        assert kwargs.get("bloom_chain"), "Please pass the thought chain."
+        inp = kwargs.get("inp")
+        thought_chain: BloomChain = kwargs.get("bloom_chain")
+        thought_memory: ChatMessageHistory = kwargs.get("thought_memory")
 
         # get the history into a string
         # history = thought_memory.load_memory_variables({})['history']
-        
+
         # response = await thought_chain.apredict(
         #     input=inp,
         #     history=history
         # )
 
-        response = thought_chain.think(thought_memory, inp)
+        response = await thought_chain.think(thought_memory, inp)
 
-        if 'Tutor:' in response:
-            response = response.split('Tutor:')[0].strip()
+        if "Tutor:" in response:
+            response = response.split("Tutor:")[0].strip()
+
+        tool_response = await thought_chain.collect_data(response, inp)
 
         return response
 
 
 class ConversationCache:
     "Wrapper Class for storing contexts between channels. Using an object to pass by reference avoid additional cache hits"
+
     def __init__(self):
         self.thought_memory, self.response_memory = load_memories()
 
-
     def restart(self):
-       self.thought_memory.clear()
-       self.response_memory.clear()
+        self.thought_memory.clear()
+        self.response_memory.clear()
